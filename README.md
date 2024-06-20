3fe4d109e93524f539f552d5cfbb80d77744c567
gaoyon01 <gaoyon01@risk.regn.net>
2024-06-20 08:43:42 -0400

    update

--- README.md
+++ README.md
@@ -8,12 +8,17 @@
 ## Getting Started
 
 * Build and run on Windows
-1. Open and edit run.ps1 to set environment variables and select producer or consumer profile.
+1. Open and edit app_env_vars.ps1 to set environment variables.
    IMPORTANT: EVENTHUB_STORAGE_ACCOUNT_KEY must be set to the storage account access key1 or key2 copied from the Azure portal.
-3. Run the script file in PowerShell 7
+3. Run producer (or consumer) script file in PowerShell 7
     ```
-    ./run.ps1
+    ./app_producer.ps1
     ```
+Or
+    ```
+    ./app_consumer.ps1
+    ```
+
 4. To test the producer app, while it's running in the PowerShell, open another PowerShell 7 session and hit the controller. For example:
     ```
     PS C:\> curl "http://localhost:8080/event?q=123"
---
+++ app_env_vars.ps1
@@ -0,0 +1,21 @@
+# This is used for AzureDefaultCredential on local box when the developer's account belongs to multiple tenants.
+$Env:AZURE_TENANT_ID="bc877e61-f6cf-4461-accd-0565fa4ca357"
+
+# When switching to application identity authentication, define following env vars:
+# $Env:AZURE_CLIENT_ID=""
+# $Env:AZURE_CLIENT_SECRET=""
+# No need to consume the vars in application.yaml, they are picked up by AzureDefaultCredential automatically.
+
+$Env:EVENTHUB_RESOURCE_GROUP="app-hearth-dev-eastus"
+
+# event hubs namespace should be only the name, without any other domain parts.
+$Env:EVENTHUB_NAMESPACE="hcfirevinedeveventhub"
+$Env:EVENTHUB_NAME="eventhub"
+
+$Env:EVENTHUB_STORAGE_ACCOUNT_NAME="hcfirevinedevsa" 
+# BEFORE RUN, COPY the storage account access key1 or key2 from the Azure portal.
+$Env:EVENTHUB_STORAGE_ACCOUNT_KEY="80MvZceN9FJwx7SAaQZN4pgX6Mjg07ggX0ak+w3Rc2Ui+iJR/61mDEK62+EioTMmuXLYFqOR5CKt+AStiIVImA==" 
+$Env:EVENTHUB_STORAGE_ACCOUNT_CONTAINER_NAME="eventstorage" 
+
+# Cannot use jdk21 with some of the current libraries/versions, change to a lower one.
+[Environment]::SetEnvironmentVariable('JAVA_HOME','C:\Program Files\Amazon Corretto\jdk17.0.11_9')
