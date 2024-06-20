87a2902aa2aea5f3d3650010ac703ab2c3b06b60
gaoyon01 <78166919+gaoyon01@users.noreply.github.com>
2024-06-19 20:00:08 -0400

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -8,12 +8,12 @@ $Env:AZURE_TENANT_ID="bc877e61-f6cf-4461-accd-0565fa4ca357"
 
 $Env:EVENTHUB_RESOURCE_GROUP="app-hearth-dev-eastus"
 
-# event hubs namespace should be only the name, not the full path
+# event hubs namespace should be only the name, without any other domain parts.
 $Env:EVENTHUB_NAMESPACE="hcfirevinedeveventhub"
 $Env:EVENTHUB_NAME="eventhub"
 
 $Env:EVENTHUB_STORAGE_ACCOUNT_NAME="hcfirevinedevsa" 
-# BEFORE RUN, COPY the storage account access key1 or key2 from the Azure portal
+# BEFORE RUN, COPY the storage account access key1 or key2 from the Azure portal.
 $Env:EVENTHUB_STORAGE_ACCOUNT_KEY="" 
 $Env:EVENTHUB_STORAGE_ACCOUNT_CONTAINER_NAME="eventstorage" 
 
@@ -22,11 +22,11 @@ $Env:EVENTHUB_STORAGE_ACCOUNT_CONTAINER_NAME="eventstorage"
 
 ./mvnw.cmd clean install
 
-# AzureCli login - needed for development with developer credential on Windows local box 
+# AzureCli login - needed for development with developer credential on Windows local box.
 az login
 
-# Set the active profile to "producer" to run the producer application
-# Set the active profile to "consumer" to run the consumer application
+# Set the active profile to "producer" to run the producer application.
+# Set the active profile to "consumer" to run the consumer application.
 $Env:ACTIVE_PROFILE="producer"
 # $Env:ACTIVE_PROFILE="consumer"
 
